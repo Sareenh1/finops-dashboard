61c7ed90d0f7a73545d434fb8d8b4d5c301c3eb8
H Sareen Kumar <sareenh10@gmail.com>
2025-07-02 23:16:24 +0530

    Update run_collector.sh

--- scripts/run_collector.sh
+++ scripts/run_collector.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/finops-dashboard
 source venv/bin/activate
-python3 scripts/aws_cost_collector.py >> logs/cost_collector.log 2>&1
+python3 scripts/aws_cost_collector.py > logs/cost_collector.log 2>&1
